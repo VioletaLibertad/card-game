8da2449351062d5b0340c92560130592a1837ecd
VioletaLibertad <morada.libertad@gmail.com>
2018-10-10 19:16:19 -0300

    Updates

---
+++ precache-manifest.75502b26f7acb48d213dbb8a42c44e1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/card-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "06015fac58d7e9563f71",
+    "url": "/card-game/static/js/runtime~main.06015fac.js"
+  },
+  {
+    "revision": "dfe9a4a7dba875cb0c8d",
+    "url": "/card-game/static/js/main.dfe9a4a7.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/card-game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "dfe9a4a7dba875cb0c8d",
+    "url": "/card-game/static/css/main.7fef6822.chunk.css"
+  },
+  {
+    "revision": "0dd77e479d1547ccd6146ca4818a0e03",
+    "url": "/card-game/index.html"
+  }
+];
