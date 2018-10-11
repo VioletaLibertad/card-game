7f6d3605012b75f121eda56f998114672254953b
VioletaLibertad <morada.libertad@gmail.com>
2018-10-11 20:00:37 -0300

    building the deck

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Cards from '../Cards/Cards';
+import './Board.css';
 
-const Boards = () => {
-  return(
-    
-  )
+class Board extends Component {
+  render() {
+    const cards = [1,2,3,4,5];
+    return(
+      <div className="board">
+        {
+          cards.map((card) => <Cards />)
+        }
+      </div>
+    )
+  }
 };
 
 export default Board;
